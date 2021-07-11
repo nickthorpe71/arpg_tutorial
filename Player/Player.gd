7cc701fe2442ecfbb80dec772b2f8dcee72ef386
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-11 15:33:53 -0500

    add tutorial link

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 var speed = 1.24
 
+# Tutorial link:
+# https://www.youtube.com/watch?v=mAbG8Oi-SvQ&list=RDCMUCrHQNOyU1q6BFEfkNq2CYMA&start_radio=1&rv=mAbG8Oi-SvQ&t=87&ab_channel=HeartBeast
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength("ui_left")
