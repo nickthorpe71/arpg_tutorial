6c897d6d25de207594deae177b0bcc26c8a257a6
Nick Thorpe <nickthorpe71@gmail.com>
2021-07-14 07:46:49 -0500

    add animation to player

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,12 @@
 extends KinematicBody2D
 
+const ACCELERATION = 700
+const MAX_SPEED = 120
+const FRICTION = 700
+
 var velocity = Vector2.ZERO
-var speed = 1.24
+
+onready var animationPlayer = $AnimationPlayer
 
 # Tutorial link:
 # https://www.youtube.com/watch?v=mAbG8Oi-SvQ&list=RDCMUCrHQNOyU1q6BFEfkNq2CYMA&start_radio=1&rv=mAbG8Oi-SvQ&t=87&ab_channel=HeartBeast
@@ -10,11 +15,21 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector * speed
+		if input_vector.x  > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
+		animationPlayer.play("IdleRight")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity)
+	
+		
+	velocity = move_and_slide(velocity)
 
